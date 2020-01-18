48630551489efa79ebeaba35523526a274713db2
spartan6nm <59691958+spartan6nm@users.noreply.github.com>
2020-01-18 09:46:35 +0330

    basic character movement

---
+++ NES JURASSIC PROJECT/Assets/Scripts/sPlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sPlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public float moveSpeed = 5f;
+
+    private float lastHMove = 0.0f;
+    private float lastVMove = 0.0f;
+
+    Vector2 movement;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        // input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+
+        if(movement.x == 0 && movement.y == 0)
+        {
+            animator.SetBool("Moving", false);
+            
+        }
+        else
+        {
+            animator.SetBool("Moving", true);
+            
+        }
+    }
+    void FixedUpdate()
+    {
+        // movement
+        rb.velocity = movement * moveSpeed * Time.fixedDeltaTime;
+    }
+}
