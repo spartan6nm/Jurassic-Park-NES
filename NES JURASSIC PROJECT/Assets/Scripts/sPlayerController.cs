6af261db6abe8d428e0da747d3fc4b078ad31284
ZaCkOX <zacko1000@hotmail.com>
2020-01-24 23:42:02 -0800

    DNA scene and bullets

--- NES JURASSIC PROJECT/Assets/Scripts/sPlayerController.cs
+++ NES JURASSIC PROJECT/Assets/Scripts/sPlayerController.cs
@@ -18,13 +18,50 @@ public class sPlayerController : MonoBehaviour
     private const string horizontal = "Horizontal";
     private const string vertical = "Vertical";
 
+    [SerializeField] private Transform Weapon;
+    [SerializeField] private float WeaponForce = 50f;
+    [SerializeField] private PooledObject Bullets;
+
+    [System.Serializable]
+    public class PooledObject {
+        //Declare
+        public GameObject Prefab;
+        public int NumberOfObjects;
+        public float Lifetime = 3.5f;
+    }
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        //Instantiate global object pools
+        zsPublicStatics.Bullets = new zsPublicStatics.gudcObject();
+
+        //Setup object pools
+        SetupObjectPoolingForBullets(zsPublicStatics.Bullets, Bullets);
+
     }
 
-    
+    void SetupObjectPoolingForBullets(zsPublicStatics.gudcObject ObjectPool, PooledObject PooledObjectToBe) {
+        //Expand
+        ObjectPool.Lifetimes = new float[PooledObjectToBe.NumberOfObjects];
+        ObjectPool.GameObjects = new GameObject[PooledObjectToBe.NumberOfObjects];
+        ObjectPool.Rigidbodies = new Rigidbody2D[PooledObjectToBe.NumberOfObjects];
+        ObjectPool.CircleColliders = new CircleCollider2D[PooledObjectToBe.NumberOfObjects];
+        //Loop
+        for (int i = 0; i < PooledObjectToBe.NumberOfObjects; i++) {
+            //Instantiate
+            ObjectPool.GameObjects[i] = Instantiate(PooledObjectToBe.Prefab, null);
+            //Set other
+            ObjectPool.Rigidbodies[i] = ObjectPool.GameObjects[i].GetComponent<Rigidbody2D>();
+            ObjectPool.CircleColliders[i] = ObjectPool.GameObjects[i].GetComponent<CircleCollider2D>();
+            //Turn off
+            ObjectPool.CircleColliders[i].enabled = false;
+            ObjectPool.GameObjects[i].SetActive(false);
+        }
+    }
+
     void Update()
     {
         // input
@@ -32,7 +69,30 @@ public class sPlayerController : MonoBehaviour
         movement.y = Input.GetAxisRaw(vertical);
 
         anim();
+
+        shoot();
+
+        CheckBulletLifetime();
+
     }
+
+    void CheckBulletLifetime() {
+
+        //Loop
+        for (int i = 0; i < Bullets.NumberOfObjects; i++) {
+            //Check if lifetimes is greater than zero
+            if (zsPublicStatics.Bullets.Lifetimes[i] > 0f) {
+                //Subtract
+                zsPublicStatics.Bullets.Lifetimes[i] -= Time.deltaTime;
+                //Check if time up
+                if (zsPublicStatics.Bullets.Lifetimes[i] <= 0f) {
+                    //Disable bullet
+                    EnableOrDisableBullet(i, false);
+                }
+            }
+        }
+    }
+
     void FixedUpdate()
     {
         // movement
@@ -59,6 +119,30 @@ public class sPlayerController : MonoBehaviour
             animator.SetBool("Moving", true);
 
         }
+    }
+
+    void shoot() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //Declare
+            int intPoolIndex = zsPublicStatics.gGetNextObjectPoolIndex(zsPublicStatics.Bullets);
+            //Reset line lifetime
+            zsPublicStatics.Bullets.Lifetimes[intPoolIndex] = Bullets.Lifetime;
+            //Position
+            zsPublicStatics.Bullets.GameObjects[intPoolIndex].transform.position = Weapon.position;
+            //Enable or disable bullet
+            EnableOrDisableBullet(intPoolIndex, true);
+            //Make bullet move
+            zsPublicStatics.Bullets.Rigidbodies[intPoolIndex].AddForce(Weapon.transform.forward * WeaponForce);
+        }
+    }
+
+    void EnableOrDisableBullet(int intPoolIndex, bool blnEnable) {
+        //Enable or disable collider
+        zsPublicStatics.Bullets.CircleColliders[intPoolIndex].enabled = blnEnable;
+        //Turn on or off game object
+        zsPublicStatics.Bullets.GameObjects[intPoolIndex].SetActive(blnEnable);
+        //Reset any forces applied
+        zsPublicStatics.Bullets.Rigidbodies[intPoolIndex].velocity = Vector2.zero;
     }
     
 }
---
+++ NES JURASSIC PROJECT/Assets/Scripts/zsPublicStatics.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class zsPublicStatics {
+
+    //Declare object pools
+    public static gudcObject Bullets;
+
+    //Declare classes
+    public class gudcObject {
+        //Declare
+        public int NextPoolNumber;
+        public float[] Lifetimes;
+        public GameObject[] GameObjects;
+        public Rigidbody2D[] Rigidbodies;
+        public CircleCollider2D[] CircleColliders;
+    }
+
+    public static int gGetNextObjectPoolIndex(gudcObject udcObject) {
+        //Declare
+        int intIndex = udcObject.NextPoolNumber;
+        //Increases and resets
+        udcObject.NextPoolNumber = (udcObject.NextPoolNumber + 1) % udcObject.Lifetimes.Length; //Checks if past the max index and resets to zero
+        //Return
+        return intIndex;
+    }
+
+}
